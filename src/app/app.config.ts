74382bb80e9c26e46d8cf9ac68dc6cbb761f002d
munees <munees@gmail.com>
2026-03-29 18:50:55 +0500

    saved2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations'; // 1. Add this import
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+     provideAnimations(), // 2. Add this to providers
     provideRouter(routes)
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,29 @@ import { Navbar } from './navbar/navbar';
 import { Child } from "./child/child";
 import { FormsModule } from '@angular/forms';
 import { NgForOf, NgIf } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, Navbar, FormsModule, Child, NgForOf, NgIf],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrls: ['./app.css'],
+  animations: [
+    trigger('FadeInOut', [
+      state('visible', style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })),
+      state('hidden', style({
+        opacity: 0,
+        transform: 'scale(0)'
+      })),
+      transition('visible <=> hidden', [
+        animate('500ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class App {
   protected readonly title = signal('first-vite-project');
@@ -21,5 +37,8 @@ export class App {
   parentData = 'data from parent';
 
   // trackBy function for ngFor
-  
+  isvisible = true;
+  toggle(){
+    this.isvisible = !this.isvisible;
+  }
 }
